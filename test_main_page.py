2d1eb56f7979437b953b628d5bef3b186924b64f
MVAnastasia <nnastymers@mail.ru>
2022-02-09 21:56:43 +0300

    Реализация LoginPage, step 4.2.8

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -10,8 +11,12 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    ##link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+def test_guest_should_see_login_page(browser):
+    page2 = LoginPage(browser, link)
+    page2.open()
+    page2.should_be_login_page()
