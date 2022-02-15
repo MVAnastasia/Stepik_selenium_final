0f81255f6b9c099b2df6b05a53937f851e8d0c53
MVAnastasia <nnastymers@mail.ru>
2022-02-15 12:12:28 +0300

    Full section 4.2

--- test_main_page.py
+++ test_main_page.py
@@ -5,10 +5,10 @@ from .pages.login_page import LoginPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+##def test_guest_can_go_to_login_page(browser):
+##    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+##    page.open()  # открываем страницу
+##    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 def test_guest_should_see_login_link(browser):
     ##link = "http://selenium1py.pythonanywhere.com/"
@@ -20,3 +20,17 @@ def test_guest_should_see_login_page(browser):
     page2 = LoginPage(browser, link)
     page2.open()
     page2.should_be_login_page()
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    login_page = page.go_to_login_page()
+    login_page.should_be_login_page()
+
+
+def test_guest_can_go_to_login_page(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
